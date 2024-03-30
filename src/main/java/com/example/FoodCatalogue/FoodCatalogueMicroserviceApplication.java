03957ec8056369d897f8c269e0a29249de9fa033
dhananjay <waradedhananjay2000@gmail.com>
2024-03-30 10:08:56 +0530

    initial file

---
+++ src/main/java/com/example/FoodCatalogue/FoodCatalogueMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.FoodCatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCatalogueMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodCatalogueMicroserviceApplication.class, args);
+	}
+
+}
