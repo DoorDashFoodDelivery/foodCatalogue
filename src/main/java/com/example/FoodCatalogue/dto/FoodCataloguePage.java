7cc41edca9787762abf80ced2652fde66a8681ee
dhananjay <waradedhananjay2000@gmail.com>
2024-03-30 13:00:21 +0530

    create foodCataloguepage where we can store list of fooditem and restaurent also we can create restaurent dto

---
+++ src/main/java/com/example/FoodCatalogue/dto/FoodCataloguePage.java
@@ -0,0 +1,19 @@
+package com.example.FoodCatalogue.dto;
+
+import java.util.List;
+
+import com.example.FoodCatalogue.entity.FoodItem;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FoodCataloguePage {
+
+	private List<FoodItem> foodItem;
+	
+	private Restaurent restaurent;
+}
