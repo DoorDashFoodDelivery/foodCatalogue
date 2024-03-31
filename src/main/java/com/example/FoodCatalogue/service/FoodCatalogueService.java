3c14b03ab9c522b700fe43ec378475fc30725d41
dhananjay <waradedhananjay2000@gmail.com>
2024-03-31 10:39:32 +0530

    Add implementation of add food item

--- src/main/java/com/example/FoodCatalogue/service/FoodCatalogueService.java
+++ src/main/java/com/example/FoodCatalogue/service/FoodCatalogueService.java
@@ -3,6 +3,9 @@ package com.example.FoodCatalogue.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.FoodCatalogue.dto.FoodItemDTO;
+import com.example.FoodCatalogue.entity.FoodItem;
+import com.example.FoodCatalogue.mapper.FoodItemMapper;
 import com.example.FoodCatalogue.repo.FoodItemRepo;
 
 @Service
@@ -10,5 +13,10 @@ public class FoodCatalogueService {
 	
 	@Autowired
 	FoodItemRepo foodItemRepo;
+
+	public FoodItemDTO addFoodItem(FoodItemDTO foodItemDTO) {
+	FoodItem foodItemSavedInDB=foodItemRepo.save(FoodItemMapper.INSTANCE.mapFoodItemDTOToFoodItem(foodItemDTO));
+		return FoodItemMapper.INSTANCE.mapFoodItemToFoodItemDTO(foodItemSavedInDB);
+	}
 	
 }
