8e0a568071535d397947fec578e7b42f1c544fc9
dhananjay <waradedhananjay2000@gmail.com>
2024-03-31 12:49:20 +0530

    create method fetchFoodCataloguePageDetails

--- src/main/java/com/example/FoodCatalogue/controller/FoodCatalogueController.java
+++ src/main/java/com/example/FoodCatalogue/controller/FoodCatalogueController.java
@@ -3,11 +3,14 @@ package com.example.FoodCatalogue.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.FoodCatalogue.dto.FoodCataloguePage;
 import com.example.FoodCatalogue.dto.FoodItemDTO;
 import com.example.FoodCatalogue.service.FoodCatalogueService;
 
@@ -23,5 +26,11 @@ public class FoodCatalogueController {
 	public ResponseEntity<FoodItemDTO> addFoodItem(@RequestBody FoodItemDTO foodItemDTO){
 		FoodItemDTO foodItemSaved=foodCatalogueService.addFoodItem(foodItemDTO);
 		return new ResponseEntity<>(foodItemSaved, HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/fetchRestaurentAndFoodItemById/{id}")
+	public ResponseEntity<FoodCataloguePage> fetchRestauDetailsWithFoodMenu(@PathVariable Integer restaurantId){
+		FoodCataloguePage foodCataloguePage=foodCatalogueService.fetchFoodCataloguePageDetails(restaurantId);
+		return new ResponseEntity<>(foodCataloguePage, HttpStatus.OK);
 	}
 }
--- src/main/java/com/example/FoodCatalogue/service/FoodCatalogueService.java
+++ src/main/java/com/example/FoodCatalogue/service/FoodCatalogueService.java
@@ -1,9 +1,13 @@
 package com.example.FoodCatalogue.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.FoodCatalogue.dto.FoodCataloguePage;
 import com.example.FoodCatalogue.dto.FoodItemDTO;
+import com.example.FoodCatalogue.dto.Restaurent;
 import com.example.FoodCatalogue.entity.FoodItem;
 import com.example.FoodCatalogue.mapper.FoodItemMapper;
 import com.example.FoodCatalogue.repo.FoodItemRepo;
@@ -17,6 +21,28 @@ public class FoodCatalogueService {
 	public FoodItemDTO addFoodItem(FoodItemDTO foodItemDTO) {
 	FoodItem foodItemSavedInDB=foodItemRepo.save(FoodItemMapper.INSTANCE.mapFoodItemDTOToFoodItem(foodItemDTO));
 		return FoodItemMapper.INSTANCE.mapFoodItemToFoodItemDTO(foodItemSavedInDB);
+	}
+
+	public FoodCataloguePage fetchFoodCataloguePageDetails(Integer restaurantId) {
+		List<FoodItem> foodItemList=fetchFoodItemList(restaurantId);
+		Restaurent restaurent=fetchRestaurantDetailsFromRestaurantMS(restaurantId);
+		createFoodCataloguePage(foodItemList,restaurent);
+		return null;
+	}
+
+	private void createFoodCataloguePage(List<FoodItem> foodItemList, Restaurent restaurent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private Restaurent fetchRestaurantDetailsFromRestaurantMS(Integer restaurantId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List<FoodItem> fetchFoodItemList(Integer restaurantId) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
